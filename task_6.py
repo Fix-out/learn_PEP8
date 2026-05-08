b5739ac91179516acc00780577567e6962262cf3
Fix-out <94598955+Fix-out@users.noreply.github.com>
2026-05-08 14:42:06 +0300

    Update task_6.py

--- task_6.py
+++ task_6.py
@@ -1,10 +1,10 @@
+def func(x = 0,  y = 0):
+    return x**2 + y**2
+    
 x = 1 + 2 * 3 - 4 / 5
 
 list1 = [1, 2, 3, 4, 5]
 dict1 = {'a': 1, 'b': 2, 'c': 3}
-
-def func(x = 0,  y = 0):
-    return x**2 + y**2
 
 result = func(2,3)
 
